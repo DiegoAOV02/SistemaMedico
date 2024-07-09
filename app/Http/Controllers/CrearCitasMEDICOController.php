f089e286b7d2afabc14565d2c5f19f0447573ad6
Diego Ortiz <2030282@upv.edu.mx>
2024-07-08 22:43:52 -0600

    Bug fixed in Crear Cita from Medico

--- app/Http/Controllers/CrearCitasMEDICOController.php
+++ app/Http/Controllers/CrearCitasMEDICOController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Paciente;
 use Illuminate\Http\Request;
 
 class CrearCitasMEDICOController extends Controller
 {
     public function index()
     {
-        return view('medico.crear-cita');
+        $pacientes = Paciente::all();
+        return view('medico.crear-cita', compact('pacientes'));
     }
 }
